6c3cbb62eea62983a4fa7d616128dc8a2155ddd2
victorpachec <161628721+victorpachec@users.noreply.github.com>
2025-09-26 17:59:44 -0300

    Add files via upload

---
+++ trabalho4bismestre/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tela de Início</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <div class="container">
+        <h1 class="titulo">Bem-vindo a etec flix</h1>
+        <div class="buttons">
+            <a href="#sobre" class="btn">Sobre</a>
+            <a href="#contato" class="btn">Contato</a>
+            <a href="#servicos" class="btn">Serviços</a>
+        </div>
+    </div>
+</body>
+</html>
